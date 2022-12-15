28fb90bb37bea6ccd26a1c654c49ef3feefebb7b
ict05-17 <ict05-17@DESKTOP-K36HJKG>
2022-12-15 15:49:59 +0900

    커밋테스트

---
+++ class/src/class1215/BasicFunction.java
@@ -0,0 +1,31 @@
+package class1215;
+
+public class BasicFunction {
+
+	// 함수 정의
+	static int f1(int x) { //매개변수 유효범위는 메서드 { } 내에서만 
+		return 3 + x * 23;
+	}
+	
+	
+	static int add(int a, int b) {
+		return a+b;
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+
+		int result = 3 + 4 * 23 - 23 + 45 + 23;
+
+		System.out.printf("result is %d\n", result);
+
+		result = f1(5) - 23 + 45 + 23 + add(5,9); // f1() : 호출
+		
+		
+		System.out.printf("result is %d\n", result);
+		
+	}
+
+}
