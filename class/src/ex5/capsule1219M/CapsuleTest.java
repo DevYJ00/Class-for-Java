740b3b9b0fe4a8ab75a7ec53625a9e26410969fd
ict05-17 <ict05-17@DESKTOP-K36HJKG>
2022-12-20 17:54:13 +0900

    1220 수업 초반

--- class/src/ex5/capsule1219M/CapsuleTest.java
+++ class/src/ex5/capsule1219M/CapsuleTest.java
@@ -6,40 +6,15 @@ public class CapsuleTest {
 
 	
 	public static void main(String[] args) {
-		//성적 객체를 준비
-		//성적 뉴렉성적 = new 성적();
-		//Exam exam1 = new Exam();
-		Exam 뉴렉성적 = new Exam();
-//		뉴렉성적.kor = 3;
-//		뉴렉성적.eng = 10;
-		뉴렉성적.init();
-		뉴렉성적.print();
 		
+		Exam  e = new Exam(30,30,50);
 		
-		//성적을 입려 받는다
-		//inputExam(뉴렉성적) <-이건 그동안 객체지향 모르고 하던 방식 사용 ㄴㄴ
-		//뉴렉성적.입력();
-		//exam1.input(); <- 객체를 이용(Exam클래스내에 input() 메서드를 만들어야함) || 객체를 이용안하면 inputExam(exam1)
-//		뉴렉성적.input(); //객체 뉴렉성적에 있는 정보가 input( )메서드로 넘어간다
-		//input() <- 인스턴스 메서드 (인스턴스를 통해서 호출되는 메서드)
+		e.print();
 		
+		Exam e2 = new Exam();
+		e2.print();
 		
-		
-		//성적을 출력한다
-		//뉴렉성적.출력();
-//		뉴렉성적.print();
-//		
-//		Exam 뉴렉성적2 = new Exam();
-//		뉴렉성적2.kor = 5;
-//		뉴렉성적2.eng = 55;
-//		뉴렉성적2.input();
-//		뉴렉성적2.print();
-//		
-//		Exam 뉴렉성적3  = new Exam(100,100,100);
-//		뉴렉성적3.input();
-//		뉴렉성적3.print();
-//		
-		
+		e.print();
 		
 	}
 
--- class/src/ex5/capsule1219M/Exam.java
+++ class/src/ex5/capsule1219M/Exam.java
@@ -8,48 +8,64 @@ public class Exam {
 	private int eng;
 	private int math;
 
-	public void init() {
-	}
-	
 	public Exam() {
-		kor = 30;
-		eng = 30;
-		math = 30;
-		
+		this(30, 30, 30);
+
 	}
-	public  Exam(int kor, int eng, int math) {
+
+	public Exam(int kor, int eng, int math) {
 		this.kor = kor;
-		this.eng  = eng;
+		this.eng = eng;
 		this.math = math;
 	}
-	
-	
-	
+
 	public void input() {
-		
-		Scanner scan  = new Scanner(System.in);
-		
+
+		Scanner scan = new Scanner(System.in);
+
 		do {
 			System.out.print("국어 : ");
 			kor = scan.nextInt();
-			
-			if(kor<0 || 100<kor)
+
+			if (kor < 0 || 100 < kor)
 				System.out.println("입력 가능 범위 : 0~100");
-		}while(kor<0 || 100<kor);
-		
-		
+		} while (kor < 0 || 100 < kor);
+
 	}
 
 	public void print() {
-		
-		
-		
-		System.out.println("영어성적 : " + eng); //eng에 this안써도 사용 가능 this의 객체를 찾아서 읽어줌
-		System.out.println("국어성적 : " + kor); //eng에 this안써도 사용 가능 this의 객체를 찾아서 읽어줌
-		System.out.println("수학성적 : " + math); //eng에 this안써도 사용 가능 this의 객체를 찾아서 읽어줌
-		
+		print('-'); // 기본값을 가지는 메소드
 	}
-	
-	
+
+	public void print(char ch, int length) {
+
+		int total = total();
+		double avg = avg(total);
+
+		System.out.println("영어성적 : " + eng);
+		System.out.println("국어성적 : " + kor);
+		System.out.println("수학성적 : " + math);
+		System.out.println("총점 : " + total);
+		System.out.println("평균성적 : " + avg);
+
+		for (int i = 0; i < length; i++)
+			System.out.printf("%c", ch);
+	}
+
+	private double avg(int total) {
+		
+		
+		return total()/3.0;
+	}
+
+	private int total() {
+		
+		return kor + eng + math;
+	}
+
+	public void print(char ch) {
+		print('-', 30);
+
+	}
 
 }
